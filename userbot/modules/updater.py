08349f5d57910f90def4f0600bda9a8bd0e7dcc2
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-19 13:45:12 +0530

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -48,7 +48,7 @@ async def upstream(ups):
     "For .update command, check if the bot is up to date, update if specified"
     await ups.edit("`Checking for updates, please wait....`")
     conf = ups.pattern_match.group(1)
-    off_repo = https://github.com/rekcah-pavi/javes
+    off_repo = UPSTREAM_REPO_URL
     force_update = False
 
     try:
