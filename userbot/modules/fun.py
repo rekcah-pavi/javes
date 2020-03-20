8277c433970f214e9550178b52858f5a96f40727
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-20 11:03:44 +0530

    Update fun.py

--- userbot/modules/fun.py
+++ userbot/modules/fun.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-""" Userbot module for having some fun with people. """
 
 from asyncio import sleep
 from random import choice, getrandbits, randint
