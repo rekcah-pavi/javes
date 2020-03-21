68abb527ce73ddf5df9c1970260dff0845efee46
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-21 09:27:59 +0530

    added blacklist  model

--- userbot/modules/group.py
+++ userbot/modules/group.py
@@ -4,6 +4,14 @@ from userbot.events import register
 from userbot import CMD_HELP, bot, LOGS, CLEAN_WELCOME, BOTLOG_CHATID
 from telethon.events import ChatAction
 from asyncio import sleep
+import asyncio
+import io
+import re
+import userbot.modules.sql_helper.blacklist_sql as sql
+from telethon import events, utils
+from telethon.tl import types, functions
+from userbot import CMD_HELP, bot
+from userbot.events import register
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 from asyncio import sleep
@@ -1603,6 +1611,67 @@ async def filters_active(event):
 
     await event.edit(transact)
 
+@register(incoming=True, disable_edited=True, disable_errors=True)
+async def on_new_message(event):
+    # TODO: exempt admins from locks
+    name = event.raw_text
+    snips = sql.get_chat_blacklist(event.chat_id)
+    for snip in snips:
+        pattern = r"( |^|[^\w])" + re.escape(snip) + r"( |$|[^\w])"
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            try:
+                await event.delete()
+            except Exception as e:
+                await event.reply("I do not have DELETE permission in this chat")
+                sql.rm_from_blacklist(event.chat_id, snip.lower())
+            break
+        pass
+
+
+@register(outgoing=True, pattern="^!addbl(?: |$)(.*)")
+async def on_add_black_list(addbl):
+    text = addbl.pattern_match.group(1)
+    to_blacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+    for trigger in to_blacklist:
+        sql.add_to_blacklist(addbl.chat_id, trigger.lower())
+    await addbl.edit("Added {} triggers to the blacklist in the current chat".format(len(to_blacklist)))
+
+
+@register(outgoing=True, pattern="^!listbl(?: |$)(.*)")
+async def on_view_blacklist(listbl):
+    all_blacklisted = sql.get_chat_blacklist(listbl.chat_id)
+    OUT_STR = "Blacklists in the Current Chat:\n"
+    if len(all_blacklisted) > 0:
+        for trigger in all_blacklisted:
+            OUT_STR += f"`{trigger}`\n"
+    else:
+        OUT_STR = "No BlackLists. Start Saving using `!addbl`"
+    if len(OUT_STR) > 4096:
+        with io.BytesIO(str.encode(OUT_STR)) as out_file:
+            out_file.name = "blacklist.text"
+            await listbl.client.send_file(
+                listbl.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="BlackLists in the Current Chat",
+                reply_to=listbl
+            )
+            await listbl.delete()
+    else:
+        await listbl.edit(OUT_STR)
+
+
+@register(outgoing=True, pattern="^!rmbl(?: |$)(.*)")
+async def on_delete_blacklist(rmbl):
+    text = rmbl.pattern_match.group(1)
+    to_unblacklist = list(set(trigger.strip() for trigger in text.split("\n") if trigger.strip()))
+    successful = 0
+    for trigger in to_unblacklist:
+        if sql.rm_from_blacklist(rmbl.chat_id, trigger.lower()):
+            successful += 1
+    await rmbl.edit(f"Removed {successful} / {len(to_unblacklist)} from the blacklist")
+    
 
 
 
