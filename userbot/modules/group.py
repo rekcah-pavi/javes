e2f93e3ccd808bad397e193a6e7feb9eff3e0a39
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-03-21 16:34:42 +0530

    Add files via upload

--- userbot/modules/group.py
+++ userbot/modules/group.py
@@ -1672,6 +1672,82 @@ async def on_delete_blacklist(rmbl):
             successful += 1
     await rmbl.edit(f"Removed {successful} / {len(to_unblacklist)} from the blacklist")
     
+@javes05(outgoing=True, pattern="^\!snip (\w*)")
+async def on_snip_save(event):
+    """ For .snip command, saves snips for future use. """
+    try:
+        from userbot.modules.sql_helper.snips_sql import add_snip
+    except AtrributeError:
+        await event.edit("`Running on Non-SQL mode!`")
+        return
+    keyword = event.pattern_match.group(1)
+    string = event.text.partition(keyword)[2]
+    msg = await event.get_reply_message()
+    msg_id = None
+    if msg and msg.media and not string:
+        if BOTLOG_CHATID:
+            await event.client.send_message(
+                BOTLOG_CHATID, f"#SNIP\
+            \nKEYWORD: {keyword}\
+            \n\nThe following message is saved as the data for the snip, please do NOT delete it !!"
+            )
+            msg_o = await event.client.forward_messages(
+                entity=BOTLOG_CHATID,
+                messages=msg,
+                from_peer=event.chat_id,
+                silent=True)
+            msg_id = msg_o.id
+        else:
+            await event.edit(
+                "`Saving snips with media requires the BOTLOG_CHATID to be set.`"
+            )
+            return
+    elif event.reply_to_msg_id and not string:
+        rep_msg = await event.get_reply_message()
+        string = rep_msg.text
+    success = "`Snip {} successfully. Use` **${}** `anywhere to get it`"
+    if add_snip(keyword, string, msg_id) is False:
+        await event.edit(success.format('updated', keyword))
+    else:
+        await event.edit(success.format('saved', keyword))
+
+
+@javes05(outgoing=True, pattern="^\!snips$")
+async def on_snip_list(event):
+    """ For .snips command, lists snips saved by you. """
+    try:
+        from userbot.modules.sql_helper.snips_sql import get_snips
+    except AttributeError:
+        await event.edit("`Running on Non-SQL mode!`")
+        return
+
+    message = "`No snips available right now.`"
+    all_snips = get_snips()
+    for a_snip in all_snips:
+        if message == "`No snips available right now.`":
+            message = "Available snips:\n"
+            message += f"`${a_snip.snip}`\n"
+        else:
+            message += f"`${a_snip.snip}`\n"
+
+    await event.edit(message)
+
+
+@javes05(outgoing=True, pattern="^\!remsnip (\w*)")
+async def on_snip_delete(event):
+    """ For .remsnip command, deletes a snip. """
+    try:
+        from userbot.modules.sql_helper.snips_sql import remove_snip
+    except AttributeError:
+        await event.edit("`Running on Non-SQL mode!`")
+        return
+    name = event.pattern_match.group(1)
+    if remove_snip(name) is True:
+        await event.edit(f"`Successfully deleted snip:` **{name}**")
+    else:
+        await event.edit(f"`Couldn't find snip:` **{name}**")
+
+
 
 
 
