fe1c92872af55d69521effd4d083216ca6b3fca1
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-04-03 18:10:21 +0530

    Update fun.py

--- userbot/modules/fun.py
+++ userbot/modules/fun.py
@@ -1358,15 +1358,15 @@ async def corona(event):
     country_data = covid.get_status_by_country_name(country)
     deaths = covid.get_total_deaths()
     if country_data:
-        output_text =  f"`Confirmed`   :    {country_data['confirmed']}\n"
-        output_text += f"`Active`          :    {country_data['active']}\n"
-        output_text += f"`Deaths`          :  {country_data['deaths']}\n"
+        output_text =  f"`Confirmed`   :   {country_data['confirmed']}\n"
+        output_text += f"`Active`          :   {country_data['active']}\n"
+        output_text += f"`Deaths`          :   {country_data['deaths']}\n"
         output_text += f"`Recovered`   :   {country_data['recovered']}\n\n"        
-        output_text += f"------`TOTAL`------\n\n"                
-        output_text += f"`Deaths`        :  {covid.get_total_deaths()}\n"
-        output_text += f"`Recovered` :   {covid.get_total_recovered()}`\n"
-        output_text += f"`Confirmed`  :    {covid.get_total_confirmed_cases()}`\n"
-        output_text += f"`Active`        :    {covid.get_total_active_cases()}\n\n"
+        output_text += f"---------TOTAL----------\n\n"                
+        output_text += f"`Deaths`          :   {covid.get_total_deaths()}\n"
+        output_text += f"`Recovered`   :   {covid.get_total_recovered()}\n"
+        output_text += f"`Confirmed`   :   {covid.get_total_confirmed_cases()}\n"
+        output_text += f"`Active`          :   {covid.get_total_active_cases()}\n\n"
         output_text += ("`Update`        :  "f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%H:%M')}[GT]\n")
     else:
         output_text = "Invalid Country name"
