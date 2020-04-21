e12622a3ec8dff90dbc855d6f5104a6e7e15fda8
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-04-21 17:44:19 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,10 +1,3 @@
 import os
-
-ENV = bool(os.environ.get("ENV", False))
-if ENV:
-    from config2 import Var as Config
-else:
-    from local_config import Development as Config
-
-
+from config2 import Var as Config
 Var = Config
