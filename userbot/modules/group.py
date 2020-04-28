eda2a6d1415ca53020e4dd977717df6d7541b1a6
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-04-28 16:48:45 +0530

    Update group.py

--- userbot/modules/group.py
+++ userbot/modules/group.py
@@ -1,5 +1,6 @@
 from asyncio import sleep
 from os import remove
+from userbot.modules.sql_helper.mute_sql import is_muted, mute, unmute
 from asyncio import sleep
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 import asyncio
@@ -4200,3 +4201,90 @@ async def ban_user(chat_id, i, rights):
         return True, None
     except Exception as exc:
         return False, str(exc)      
+
+
+
+from userbot.modules.sql_helper.mute_sql import is_muted, mute, unmute
+import asyncio
+
+@command(outgoing=True, pattern=r"^.gmute ?(\d+)?")
+async def startmute(event):
+    private = False
+    if event.fwd_from:
+        return
+    elif event.is_private:
+        await event.edit("Grabs a huge, sticky duct tape!")
+        await asyncio.sleep(3)
+        private = True
+    if any([x in event.raw_text for x in ("/mute", "!mute")]):
+        await asyncio.sleep(0.5)
+    else:
+        reply = await event.get_reply_message()
+        if event.pattern_match.group(1) is not None:
+            userid = event.pattern_match.group(1)
+        elif reply is not None:
+            userid = reply.sender_id
+        elif private is True:
+            userid = event.chat_id
+        else:
+            return await event.edit("Please reply to a user or add their userid into the command to mute them.")
+        chat_id = event.chat_id
+        chat = await event.get_chat()
+        if "admin_rights" in vars(chat) and vars(chat)["admin_rights"] is not None: 
+            if chat.admin_rights.delete_messages is True:
+                pass
+            else:
+                return await event.edit("`You can't mute a person if you dont have delete messages permission.")
+        elif "creator" in vars(chat):
+            pass
+        elif private == True:
+            pass
+        else:
+            return await event.edit("`You can't mute a person without admin rights ")
+        if is_muted(userid, chat_id):
+            return await event.edit("This user is already muted in this chat ")
+        try:
+            mute(userid, chat_id)
+        except Exception as e:
+            await event.edit("Error occured!\nError is " + str(e))
+        else:
+            await event.edit("Successfully muted that person.")
+
+@command(outgoing=True, pattern=r"^.ungmute ?(\d+)?")
+async def endmute(event):
+    private = False
+    if event.fwd_from:
+        return
+    elif event.is_private:
+        await event.edit("Removing Tape !!")
+        await asyncio.sleep(3)
+        private = True
+    if any([x in event.raw_text for x in ("/unmute", "!unmute")]):
+        await asyncio.sleep(0.5)
+    else:
+        reply = await event.get_reply_message()
+        if event.pattern_match.group(1) is not None:
+            userid = event.pattern_match.group(1)
+        elif reply is not None:
+            userid = reply.sender_id
+        elif private is True:
+            userid = event.chat_id
+        else:
+            return await event.edit("Please reply to a user or add their userid into the command to unmute them.")
+        chat_id = event.chat_id
+        if not is_muted(userid, chat_id):
+            return await event.edit("This user is not muted in this chat")
+        try:
+            unmute(userid, chat_id)
+        except Exception as e:
+            await event.edit("Error occured!\nError is " + str(e))
+        else:
+            await event.edit("Successfully unmuted that person")
+            
+
+
+@command(incoming=True)
+async def watcher(event):
+    if is_muted(event.sender_id, event.chat_id):
+        await event.delete()
+
