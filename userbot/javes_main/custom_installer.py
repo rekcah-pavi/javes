4d4d2772f893ebfc608628eb04adf0259855c56e
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-05-19 11:19:31 +0530

    Update custom_installer.py

--- userbot/javes_main/custom_installer.py
+++ userbot/javes_main/custom_installer.py
@@ -61,3 +61,37 @@ async def load(event):
         await event.edit(f"Could not load {shortname} because of the following error.\n{str(e)}")
 
 
+@command(pattern="^!send (?P<shortname>\w+)$", outgoing=True)
+async def send(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    input_str = event.pattern_match["shortname"]
+    the_plugin_file = "./userbot/modules/{}.py".format(input_str)
+    start = datetime.now()
+    await event.client.send_file(  # pylint:disable=E0602
+        event.chat_id,
+        the_plugin_file,
+        force_document=True,
+        allow_cache=False,
+        reply_to=message_id
+    )
+    end = datetime.now()
+    time_taken_in_ms = (end - start).seconds
+    await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
+    await asyncio.sleep(DELETE_TIMEOUT)
+    await event.delete()
+
+
+@command(pattern="^!unload (?P<shortname>\w+)$", outgoing=True)
+async def unload(event):
+    if event.fwd_from:
+        return
+    shortname = event.pattern_match["shortname"]
+    try:
+        remove_plugin(shortname)
+        await event.edit(f"Unloaded {shortname} successfully")
+    except Exception as e:
+        await event.edit("Successfully unload {shortname}\n{}".format(shortname, str(e)))
+
+
