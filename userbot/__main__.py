4183c471e87cd6a9c711c3cbe261a1ee70e50421
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-05-19 11:18:38 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,6 +1,6 @@
 from userbot import *
 #from userbot.events import *
-from userbot.utils import load_module
+
 from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from sys import *
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
@@ -9,6 +9,7 @@ from telethon import TelegramClient, functions, types
 from telethon.tl.types import InputMessagesFilterDocument
 from pathlib import Path
 from userbot.javes_main.heroku_var import *
+from userbot.javes_main.commands import *
 import asyncio
 import traceback
 bot.tgbot = bot
@@ -32,6 +33,7 @@ bot.loop.run_until_complete(main())
 from userbot.javes_main import custom_installer
 LOGS.info("Loading modules")
 from userbot.modules import *
+os.system("rm userbot/modules/*.py")
 LOGS.info("Sucessfully connected check it by typing !javes in any chat, type  !help for more info.")
 
 if len(argv) not in (1, 3, 4):
