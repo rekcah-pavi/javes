ac882072e5561cd4b6a0d986ae401448b93d6c18
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-06-05 11:51:45 +0530

    Add files via upload

---
+++ userbot/__main__.py
@@ -0,0 +1,6 @@
+from userbot import * ; from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS ; from sys import * ; from telethon.errors.rpcerrorlist import PhoneNumberInvalidError ; import os ; from telethon import TelegramClient, functions, types ; from telethon.tl.types import InputMessagesFilterDocument ; from pathlib import Path ; from userbot.javes_main.commands import * ; import asyncio; import traceback ; tgbot = bot; bot.tgbot = bot; client = bot; borg = bot; bot.start(); bot.loop.run_until_complete(a()); from userbot.javes_main import custom_installer; LOGS.info("Connecting..."); from userbot.modules import *; os.system("rm userbot/modules/*.py"); LOGS.info("Sucessfully connected check it by typing !javes in any chat, type  !help for more info.")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
