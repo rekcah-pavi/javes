1d372c132f67e746672efb9b9e234e33e1fbf366
rekcah-pavi <55050467+rekcah-pavi@users.noreply.github.com>
2020-07-03 08:52:54 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,19 +1,23 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
+print ("")
+print ("")
+print("""processing.......""")
 
-print("""Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details""")
-
-API_KEY = input("API_KEY: ")
-API_HASH = input("API_HASH: ")
-
-
-with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-    print("Here is your userbot srting, copy it to a safe place !!")
-    print("")
-    print(client.session.save())
-    print("")
-    print("")
-    print("Enjoy your javes !!")
+API_KEY = '1754367'
+API_HASH = "231b8cc6cca12ee51a85cf543321f476"
+while True:
+  try:
+   with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+      print("")
+      session = client.session.save()
+      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(tap to copy)👇 \n\n `{session}`")
+      print("You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages ")
+      print("Store it safe !!")
+  except:
+   print ("")
+   print ("Wrong phone number \n make sure its with correct  country code")
+   print ("")
+   continue
+  break
+    
